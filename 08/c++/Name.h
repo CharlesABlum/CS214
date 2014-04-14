4a6fe9fe40840f894ec56387eb62702fadaef7b2
Charles Blum <theo@theo-VBox.(none)>
2014-04-14 08:14:37 -0400

    done with lab 08

--- 08/c++/Name.h
+++ 08/c++/Name.h
@@ -1,7 +1,25 @@
 /* Name.h declares class Name.
  *
  * Begun by: Dr. Adams, CS 214 at Calvin College.
- * Completed by:
- * Date:
+ * Completed by:Charles Blum
+ * Date: 13 April 2014
  */
 
+#include <string>          // string
+using namespace std;
+
+class Name {
+public:                          // interface functions
+   Name(const string & first, const string & middle, const string &
+last);
+   void print(ostream & out);
+   string getFirst() const;
+   string getMiddle() const;
+   string getLast() const;
+   string getFullName() const;
+
+private:
+   string myFirst,
+          myMiddle,
+          myLast;
+};
--- 08/e-lisp/namer.el
+++ 08/e-lisp/namer.el
@@ -1,65 +1,18 @@
 ;;; namer.el simulates and tests an e-LISP Name type.
 ;;;
 ;;; Begun by: Dr. Adams, CS 214 at Calvin College.
-;;; Completed by:
-;;; Date:
+;;; Completed by: Charles Blum
+;;; Date: 13 April 2014
 ;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Name constructs a name from three strings.      ;;
-;;; Receive: first, middle and last, three strings. ;;
-;;; Return: the triplet (first middle last).        ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Load the Name module                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path  ".")
+("." "/usr/share/emacs23/site-lisp/dictionaries-common" "/etc/emacs23" "/etc/emacs" "/usr/local/share/emacs/23.3/site-lisp" "/usr/local/share/emacs/site-lisp" "/usr/share/emacs/23.3/site-lisp" "/usr/share/emacs/23.3/site-lisp/dictionaries-common" "/usr/share/emacs/site-lisp" "/usr/share/emacs/23.3/leim" "/usr/share/emacs/23.3/lisp" "/usr/share/emacs/23.3/lisp/url" ...)
 
-(defun Name(first middle last)
-  (list first middle last) )
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; getFirst extracts the first name of a name object.;
-;;; Receive: theName, a Name.                       
-;;; Return: the first string in theName.           
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun getFirst(theName)
-  (car theName) )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; getMiddle extracts the middle name of a name object.
-;;; Receive: theName, a Name.                      
-;;; Return: the second string in theName.           
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun getMiddle(theName)
-  (car (cdr theName) ) )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; getLast extracts the last name of a name object.
-;;; Receive: theName, a Name.                    
-;;; Return: the third string in theName.       
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun getLast(theName)
-  (car (cdr (cdr theName) ) ) )
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; getFullName returns a full name in F-M-L order. 
-;;; Return: myFirst, myMiddle, myLast.           
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun getFullName (theName)
-   (concat (getFirst theName) " " (getMiddle theName) " " (getLast theName) ))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; printName displays a name object.         
-;;; Receive: theName, a Name,                 
-;;;          buffer, the name of a buffer.   
-;;; Output: the strings in theName to buffer. ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defun printName (theName buf)
-   (princ (getFullName theName) buf)
-   theName )
+(load "Name.elc")
+t
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -80,3 +33,6 @@
   (princ "All tests passed!")                ; feedback
   (terpri buf))                              ; newline
 
+John Paul Jones
+All tests passed!
+t

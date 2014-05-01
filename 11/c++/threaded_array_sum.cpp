c65084d7cec5d6d703533afc28c2f059e11ba17b
Charles Blum <theo@theo-VBox.(none)>
2014-05-01 08:41:15 -0400

    Ada edits, pushsh to use in Ulab

--- 11/c++/threaded_array_sum.cpp
+++ 11/c++/threaded_array_sum.cpp
@@ -1,5 +1,6 @@
-/* array_sum.cpp sums the values in a file
+/* threaded_array_sum.cpp sums the values in a file
  *  by reading them into an array and summing them there.
+ *  It splits the work across several threads to increase performance.
  *
  * Begun by: Prof. Adams, for CS 214 at Calvin College.
  * Completed by:Charles Blum
@@ -15,11 +16,12 @@
 #include <cstdlib>      // exit()
 #include "NanoTimer.h"  // nanosecond timer
 #include <iomanip>      // formatted output
+#include <omp.h>	// OmpenMP
 using namespace std;
 
 void checkCommandLine(int argc);
 void readFile(const string& fileName, vector<int>& v);
-long sum(const vector<int>& v);
+long sum(const vector<int>& v, unsigned numThreads);
 void display(long total, const NanoTimer& nTimer);
 
 int main(int argc, char** argv) {
@@ -29,21 +31,22 @@ int main(int argc, char** argv) {
    long total;
    NanoTimer nTimer;
    nTimer.start();
-   total = sum(values);
+   total = sum( values, atoi(argv[2]) );
    nTimer.stop();
    display(total, nTimer);
 }
 
 void checkCommandLine(int argc) {
-   if (argc != 2) {
-      cerr << "\nUsage: array_sum <fileName>\n\n";
+   if (argc != 3) {
+      cerr << "\nUsage: array_sum <fileName> <numThreads>\n\n";
       exit(1);
    }
 }
 
-long sum(const vector<int>& v) {
+long sum(const vector<int>& v, unsigned numThreads) {
    long result = 0;
 
+   #pragma omp parallel for num_threads(numThreads) reduction(+ : result)
    for (unsigned i = 0; i < v.size(); i++) {
       result += v[i];
    }
